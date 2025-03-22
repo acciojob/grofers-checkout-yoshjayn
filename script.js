44bf31e75529981d58c1002fe9074063ba24e267
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-22 23:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
-};
+            //Add your code here
+            let prices = document.querySelectorAll(".price");
+            // console.log(prices)
+            let total = 0
+            for (p of prices) {
+                total += + p.innerText;
+            }
+            console.log(total)
+
+            
+            const totalRow = document.createElement("tr");
+            const totalValue = document.createElement("td");
+            
+            totalValue.innerText=total
+            totalRow.append(totalValue)
+            let table=document.querySelector('table')
+            table.append(totalRow)
+        };
 
 getSumBtn.addEventListener("click", getSum);
 
